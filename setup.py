9a895f321ef2ec90af88bdb9b62b6d7a787dfb91
lancelotimb <lancelot.imb@gmail.com>
2021-11-05 16:21:51 +0100

    Update requirements

--- setup.py
+++ setup.py
@@ -11,14 +11,16 @@ setup(
     url='https://github.com/lancelotimb/dd-graphs',  # Provide either the link to your github or to your website
     download_url='https://github.com/lancelotimb/dd-graphs/archive/refs/tags/v_0.12.tar.gz',  # I explain this later on
     keywords=[],  # Keywords that define your package best
-    install_requires=[],
+    install_requires=[
+        'ipython~=7.29.0'
+    ],
     classifiers=[
         'Development Status :: 3 - Alpha',
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Developers',  # Define that your audience are developers
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',  # Again, pick a license
-        'Programming Language :: Python :: 3',  # Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3',  # Specify which python versions that you want to support
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
