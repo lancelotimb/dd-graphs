ff6f6f2efcdf9e3784c465c8977b41543654b4f0
Romain Mondon-Cancel <romain.mondoncancel@datadoghq.com>
2021-11-26 09:32:43 +0100

    Fix function name

--- DDGraphs/__init__.py
+++ DDGraphs/__init__.py
@@ -1,6 +1,6 @@
 from .init import init
 from .draw_timeseries_graph import draw_timeseries_graph
-from .draw_pie_charts import draw_pie_charts
+from .draw_pie_charts import draw_pie_chart
 from .draw_top_list import draw_top_list
 
-__all__ = ["init", "draw_timeseries_graph", "draw_pie_charts", "draw_top_list"]
+__all__ = ["init", "draw_timeseries_graph", "draw_pie_chart", "draw_top_list"]
